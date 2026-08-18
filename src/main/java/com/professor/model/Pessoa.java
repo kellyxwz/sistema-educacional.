08f5cf99b5d94a5a7c9094f6cdb7831cf101360e
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-08-18 18:58:52 -0300

    feat: criado a entidade professor e seus atributos

---
+++ src/main/java/com/professor/model/Pessoa.java
@@ -0,0 +1,24 @@
+package com.professor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "pessoa")
+public class Pessoa {
+
+    @Id
+    private String id;
+
+    private String name;
+    private Integer idade;
+    private String email;
+    boolean ativo;
+}
