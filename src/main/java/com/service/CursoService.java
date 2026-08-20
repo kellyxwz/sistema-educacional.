cfc1de05bf38b82cddfff2b8eaab90052f3ca02f
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-08-20 14:59:06 -0300

    feat: criado CursoService

---
+++ src/main/java/com/service/CursoService.java
@@ -0,0 +1,65 @@
+package com.service;
+
+import com.dto.request.CursoRequestDTO;
+import com.dto.response.CursoResponseDTO;
+
+
+import com.model.Curso;
+import com.repository.CursoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CursoService {
+
+    private final CursoRepository cursoRepository;
+
+    public CursoService(CursoRepository cursoRepository) {
+        this.cursoRepository = cursoRepository;
+    }
+
+    public List<CursoResponseDTO> findAll(){
+        return cursoRepository.findAll().stream().map(CursoResponseDTO :: new).toList();
+    }
+
+    public CursoResponseDTO findById(String id){
+        Curso curso = cursoRepository.findById(id).orElseThrow(() -> new RuntimeException("Curso não encontrada com o id: " + id));
+        return new CursoResponseDTO(curso);
+    }
+
+    public CursoResponseDTO create(CursoRequestDTO requestDTO){
+        Curso curso = cursoRepository.save(toEntity(requestDTO));
+
+        return new CursoResponseDTO(curso);
+    }
+
+    public CursoResponseDTO update(CursoRequestDTO requestDTO, String id){
+        Curso curso = cursoRepository.findById(id).orElseThrow(()->new RuntimeException("curso não encontrada"));
+
+        updateData(curso,requestDTO);
+
+        return new CursoResponseDTO(curso);
+    }
+
+    public void deleteById(String id){
+        cursoRepository.deleteById(id);
+    }
+
+    private void updateData(Curso curso, CursoRequestDTO dto){
+        curso.setNome(dto.nome());
+        curso.setDescricao(dto.descricao());
+        curso.setCargaHoraria(dto.cargaHoraria());
+    }
+
+    public static Curso toEntity(CursoRequestDTO dto){
+        Curso curso = new Curso();
+        curso.setNome(dto.nome());
+        curso.setDescricao(dto.descricao());
+        curso.setCargaHoraria(dto.cargaHoraria());
+        curso.setAtivo(true);
+        return curso;
+    }
+
+
+}
