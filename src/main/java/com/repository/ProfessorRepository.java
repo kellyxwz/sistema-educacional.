d8a6b6323e68d425c53e299af42f937454647627
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-09-01 22:38:22 -0300

    feat: adicionando a interface JpaSpecificationExecutor nos repositorios das entidades para a busca Avançada

--- src/main/java/com/repository/ProfessorRepository.java
+++ src/main/java/com/repository/ProfessorRepository.java
@@ -2,6 +2,7 @@ package com.repository;
 
 import com.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+public interface ProfessorRepository extends JpaRepository<Professor, Long>, JpaSpecificationExecutor<Professor> {
 }
